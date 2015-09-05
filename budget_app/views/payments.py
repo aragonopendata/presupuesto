a7e690fe95cc79e44b55a8a6a506c0fbe83546b4
David Cabo <david.cabo@gmail.com>
2015-09-05 14:49:48 +0200

    Make payment page support multiple years

--- budget_app/views/payments.py
+++ budget_app/views/payments.py
@@ -11,18 +11,21 @@ def payments(request, render_callback=None):
     c['payee_breakdown'] = BudgetBreakdown(['payee', 'area', 'description'])
     c['area_breakdown'] = BudgetBreakdown(['area', 'payee', 'description'])
 
-    budget = populate_latest_budget(c)
-    # TODO: We should have a year slider in the page, but in the meantime...
-    c['year'] = budget.year
+    # FIXME: remove hardcoded years
+    c['years'] = ['2014', '2015']
+    c['latest_year'] = '2015'
 
-    for item in Payment.objects.each_denormalized("b.id = %s", [budget.id]):
+    for item in Payment.objects.each_denormalized():
         # We add the date to the description, if it exists:
         # TODO: I wanted the date to be in a separate column, but it's complicated right
         # now the way BudgetBreakdown works. Need to think about it
         if item.date:
             item.description = item.description + ' (' + str(item.date) + ')'
 
-        c['payee_breakdown'].add_item(budget.year, item)
-        c['area_breakdown'].add_item(budget.year, item)
+        c['payee_breakdown'].add_item(item.year, item)
+        c['area_breakdown'].add_item(item.year, item)
+
+    # Additional data needed by the view
+    populate_stats(c)
 
     return render_to_response('payments/index.html', c)
