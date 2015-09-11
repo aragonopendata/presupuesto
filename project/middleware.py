73bf6e79ab8acd4b38a0e388a62a2c38b9e5af8c
David Cabo <david.cabo@gmail.com>
2015-09-11 17:08:28 +0200

    Add comment

--- project/middleware.py
+++ project/middleware.py
@@ -2,6 +2,9 @@ from django.middleware.cache import UpdateCacheMiddleware
 
 import re
 
+# We need to remove Google Analytics cookies or they'll break the cache
+# as soon as there's a "Vary: Cookie" header, which is our case.
+# See https://github.com/aragonopendata/presupuesto/issues/12 for more info.
 class SmartUpdateCacheMiddleware(UpdateCacheMiddleware):
     STRIP_RE = re.compile(r'\b(_[^=]+=.+?(?:; |$))')
 
