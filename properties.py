99c9a5602468e9f51e605b21c685d56924865bfc
Unknown <galcober@deloitte.es>
2017-03-20 15:49:12 +0100

    Add Properties.py
    In order to locate all config. variables

---
+++ properties.py
@@ -0,0 +1,13 @@
+# Properties
+
+# GLOBAL
+urlOpenData = 'http://opendata.aragon.es'
+appCkan = '/datos'
+urlCodeRepo = 'https://github.com/aragonopendata/presupuesto'
+urlLicenseRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/LICENSE.md'
+urlInstallRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/INSTALL.md'
+urlCivioFundation = 'http://dondevanmisimpuestos.es/'
+draftBudgetYear = '2014'
+
+# /reuse
+urlCatalogoPresupuesto = urlOpenData + appCkan + '/busqueda-libre/Presupuesto'
