d9f1b8e1203e5d003966764260668671f1ffe7e7
Unknown <galcober@deloitte.es>
2017-03-20 15:50:19 +0100

    Change the Draft Budget Year
    Change the Draft Budget Year to 2017

--- properties.py
+++ properties.py
@@ -7,7 +7,7 @@ urlCodeRepo = 'https://github.com/aragonopendata/presupuesto'
 urlLicenseRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/LICENSE.md'
 urlInstallRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/INSTALL.md'
 urlCivioFundation = 'http://dondevanmisimpuestos.es/'
-draftBudgetYear = '2014'
+draftBudgetYear = '2017'
 
 # /reuse
 urlCatalogoPresupuesto = urlOpenData + appCkan + '/busqueda-libre/Presupuesto'
