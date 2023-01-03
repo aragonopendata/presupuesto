9f7bd9bd38a42f1d107551a9801c656d4b3de55f
opendata-aragon <opendata@aragon.es>
2023-01-03 11:33:47 +0100

    properties for draft 2023 and template notification added

--- properties.py
+++ properties.py
@@ -7,7 +7,7 @@ urlCodeRepo = 'https://github.com/aragonopendata/presupuesto'
 urlLicenseRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/LICENSE.md'
 urlInstallRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/INSTALL.md'
 urlCivioFundation = 'http://dondevanmisimpuestos.es/'
-draftBudgetYear = '2022'
+draftBudgetYear = '2023'
 draftBudgetYear_2 = '2020'
 
 # /reuse
