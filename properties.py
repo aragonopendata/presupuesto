3c245f66d8f5043d4a2c6c0d6977fef63e74919d
opendata aragon <opendata@aragon.es>
2025-04-01 17:25:52 +0200

    budget yeard updated

--- properties.py
+++ properties.py
@@ -7,8 +7,8 @@ urlCodeRepo = 'https://github.com/aragonopendata/presupuesto'
 urlLicenseRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/LICENSE.md'
 urlInstallRepo = 'https://github.com/aragonopendata/presupuesto/blob/master/INSTALL.md'
 urlCivioFundation = 'http://dondevanmisimpuestos.es/'
-draftBudgetYear = '2022'
-draftBudgetYear_2 = '2020'
+draftBudgetYear = '2024'
+draftBudgetYear_2 = '2023'
 
 # /reuse
 urlCatalogoPresupuesto = urlOpenData + appCkan + '/busqueda-libre/Presupuesto'
